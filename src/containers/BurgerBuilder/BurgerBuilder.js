fcf913aef133a9dfb02a01635ab83a414c2d6b45
Gabrielle <qwy0129@gmail.com>
2020-12-03 16:36:57 +0800

    send data to sever

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,32 +87,11 @@ class BurgerBuilder extends  Component {
     }
 
     purchaseContinueHandler = () => {
-        // this.setState( { loading: true } );
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice.toFixed(2),
-        //     customer: {
-        //         name: 'Gabrielle',
-        //         address: {
-        //             street: 'Teststreet 1',
-        //             zipCode: '41351',
-        //             country: 'Canada'
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        // .then( response => {
-        //     this.setState( {loading: false, purchasing: false} );
-        // })
-        // .catch(error => {
-        //     this.setState( {loading: false, purchasing: false} );
-        // });
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
